cf237315abfaeaf517c0f03ced4f0b5be9dcbd67
lucasfelber <lucas.felber@inode.at>
2024-07-02 15:11:01 +0200

    contenteditable and number component

--- src/components/Number.vue
+++ src/components/Number.vue
@@ -1,37 +1,132 @@
 <template>
-    <div class="number-wrapper" :draggable="preview">
-        <input class="number" type="number" v-model="number">
+    <div class="number-wrapper" :class="{warning}">
+        <button class="counter" @click="number--"></button>
+        <div class="number-border" :class="{warning}">
+            <div class="number-box">
+                <contenteditable
+                    class="number"
+                    tag="span"
+                    v-model.number="number"
+                    :contenteditable="true"
+                    :no-html="true"
+                    :no-nl="true"
+                    @keypress="checkNumber"
+                ></contenteditable>
+                <!-- <span ref="number" class="number" @keypress="checkNumber" contenteditable="true"></span> -->
+            </div>
+        </div>
+        <button class="counter" @click="number++"></button>
     </div>
 </template>
 
+<script setup lang="ts">
+// TODO: use this as long as this is not implemented:
+// https://developer.mozilla.org/en-US/docs/Web/CSS/field-sizing
+import contenteditable from 'vue-contenteditable';
+
+const warning = ref(false);
+// TODO: because contenteditable needs a string this will log a warning
+// however it needs to be a number for math operations
+const number = ref(Number.MAX_VALUE);
+
+function checkNumber(event: KeyboardEvent) {
+    if (isNaN(Number(event.key)) || number.value > 10) {
+        event.preventDefault();
+        warning.value = true;
+        setTimeout(() => {
+            warning.value = false;
+        }, 1000);
+    }
+}
+</script>
+
 <style scoped>
+/* TODO: will need this if actually using inputs */
 /* Removes arrows on number input */
-input::-webkit-outer-spin-button,
+/* input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
   -webkit-appearance: none;
   margin: 0;
-}
+} */
 
 .number-wrapper {
     display: flex;
-    background-color: blue;
-    /* height: 1em; */
+    align-items: center;
+    justify-content: left;
+}
+
+.number-wrapper.warning {
+    animation: shake 0.2s ease-in-out 0s 3;
+}
+
+.number-border {
+    --hex-min-height: 2rem;
+    --hex-min-width: calc(var(--hex-min-height) * (1/cos(30deg)));
+    --hex-border-padding: 2px;
+
+    padding: var(--hex-border-padding);
+    background-color: black;
+
+    clip-path: polygon(
+        0% calc(100% - calc(var(--hex-min-height) / 2)),
+        0% calc(var(--hex-min-height) / 2),
+        calc(var(--hex-min-width) / 2 + var(--hex-border-padding)) calc(var(--hex-min-height) / -2),
+        calc(100% - calc(var(--hex-min-width) / 2) - var(--hex-border-padding)) calc(var(--hex-min-height) / -2),
+        100% calc(var(--hex-min-height) / 2),
+        100% calc(100% - (var(--hex-min-height) / 2)),
+        calc(100% - calc(var(--hex-min-width) / 2) - var(--hex-border-padding)) calc(100% + calc(var(--hex-min-height) / 2)),
+        calc(var(--hex-min-width) / 2 + var(--hex-border-padding)) calc(100% + calc(var(--hex-min-height) / 2))
+    );
+    transition: background-color 0.2s;
+}
+
+.number-border.warning {
+    animation: turn-red 1s linear 0s 1;
+}
+
+@keyframes shake {
+    0% {margin-left: 0rem;}
+    25% {margin-left: 0.5rem;}
+    75% {margin-left: -0.5rem;}
+    100% {margin-left: 0rem;}
+}
+
+@keyframes turn-red {
+    0% {background-color: red;}
+    90% {background-color: red;}
+}
+
+.number-box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #fffff4;
+    min-height: var(--hex-min-height);
+    min-width: var(--hex-min-width);
+    max-width: calc(var(--hex-min-width) * 4);
+    clip-path: polygon(
+        0% calc(100% - calc(var(--hex-min-height) / 2)),
+        0% calc(var(--hex-min-height) / 2),
+        calc(var(--hex-min-width) / 2) calc(var(--hex-min-height) / -2),
+        calc(100% - calc(var(--hex-min-width) / 2)) calc(var(--hex-min-height) / -2),
+        100% calc(var(--hex-min-height) / 2),
+        100% calc(100% - (var(--hex-min-height) / 2)),
+        calc(100% - calc(var(--hex-min-width) / 2)) calc(100% + calc(var(--hex-min-height) / 2)),
+        calc(var(--hex-min-width) / 2) calc(100% + calc(var(--hex-min-height) / 2))
+    );
 }
 
 .number {
-    min-width: 1em;
-    background-color: transparent;
-    margin: 0;
+    overflow-wrap: break-word;
+    max-inline-size: calc(var(--hex-min-width) * 4);
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+}
+
+.counter {
+    width: 0.75rem;
+    height: 0.75rem;
     padding: 0;
-    outline: none;
-    border: 0;
+    border: black solid 1px;
 }
 </style>
-
-<script setup lang="ts">
-defineProps<{
-    preview?: boolean
-}>()
-
-const number = ref(0)
-</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-    <div :style="{width: numWidth + 'rem'}" class="new-number"></div>
-
-    <input type="range" min="0" max="10" v-model="numWidth">
+    <Number></Number>
     <!-- <div class="new-number-wrapper">
         <div class="new-number-triange-left"></div>
         <div class="new-number">3</div>
@@ -36,8 +34,6 @@
 </template>
 
 <script setup lang="ts">
-const numWidth = ref(2)
-
 function handleDragStart(event: DragEvent) {
     if (event.target instanceof Element) {
         event.dataTransfer?.setData("text", event.target.id)
@@ -53,52 +49,4 @@ function handleDrop(event: DragEvent) {
 </script>
 
 <style scoped>
-
-.new-number {
-    --hex-height: 2rem;
-    --hex-min-width: calc(var(--hex-height) * (1/cos(30deg)));
-    background-color: green;
-    height: var(--hex-height);
-    min-width: var(--hex-min-width);
-    clip-path: polygon(
-        0% 50%,
-        calc(var(--hex-min-width) / 2) -50%,
-        calc(100% - calc(var(--hex-min-width) / 2)) -50%,
-        100% 50%,
-        calc(100% - calc(var(--hex-min-width) / 2)) 150%,
-        calc(var(--hex-min-width) / 2) 150%
-    );
-}
-
-
-.input-sizer {
-    display: inline-grid;
-}
-
-.preview-wrapper {
-    display: flex;
-    background-color: gray;
-}
-
-.preview-item {
-    margin: 10px;
-    padding: 10px;
-}
-
-.text {
-    border: 2px solid blue;
-    height: 2em;
-    width: 100px;
-}
-
-.number {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 48px;
-    height: 48px;
-    background: red;
-    -webkit-clip-path: polygon(50% 0%, 95% 25%, 95% 75%, 50% 100%, 5% 75%, 5% 25%);
-    clip-path: polygon(50% 0%, 95% 25%, 95% 75%, 50% 100%, 5% 75%, 5% 25%);
-}
 </style>
