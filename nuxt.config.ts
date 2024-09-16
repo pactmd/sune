9e718acf9f4667e1618b3cee17e403f885875975
lucasfelber <lucas.felber@inode.at>
2024-09-16 23:29:01 +0200

    minimal setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-07-11',
   srcDir: 'src',
   devtools: { enabled: true },
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+    srcDir: "src",
+    manifest: {
+      name: "Sune",
+      theme_color: "#000000",
+      icons: [
+        {
+          src: "icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true,
+      type: "module",
+    },
+  }
 });
