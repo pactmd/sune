fe37537dbf8055e0ec762f4645106c20416c2313
lucasfelber <lucas.felber@inode.at>
2024-09-17 23:45:49 +0200

    richer setup & nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,50 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-11',
-  srcDir: 'src',
+  compatibilityDate: '2024-04-03',
+
+  future: {
+    compatibilityVersion: 4
+  },
+
   devtools: { enabled: true },
-  modules: ['@vite-pwa/nuxt'],
-  pwa: {
-    srcDir: "src",
-    manifest: {
-      name: "Sune",
-      theme_color: "#000000",
-      icons: [
-        {
-          src: "icon-144x144.png",
-          sizes: "144x144",
-          type: "image/png",
-        },
-      ],
+
+  nitro: {
+    prerender: {
+      routes: ['/'],
     },
+  },
+
+  modules: ['@vite-pwa/nuxt'],
+  
+  pwa: {
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Sune',
+      short_name: 'Sune',
+      description: 'Description of Sune',
+      theme_color: '#000000',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
     devOptions: {
       enabled: true,
-      type: "module",
+      suppressWarnings: false,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
     },
-  }
+
+    registerWebManifestInRouteRules: true,
+  },
 });
