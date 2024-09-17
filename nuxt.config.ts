8d55081b618897b49d9150ca323c67ae69bdea69
lucasfelber <56022081+lucasfelber@users.noreply.github.com>
2024-09-17 23:57:28 +0200

    PWA switch (#3)
    * reverted to pure nuxt
    
    * Update README
    
    * PWA setup & nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,50 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-11',
+  compatibilityDate: '2024-04-03',
 
-  srcDir: 'src',
+  future: {
+    compatibilityVersion: 4
+  },
 
-  // (optional) Enable the Nuxt devtools
   devtools: { enabled: true },
 
-  // Enable SSG
-  ssr: false,
-
-  // Enables the development server to be discoverable by other devices for mobile development
-  devServer: { host: '0.0.0.0'},
-
-  vite: {
-    // Better support for Tauri CLI output
-    clearScreen: false,
-    // Enable environment variables
-    // Additional environment variables can be found at
-    // https://tauri.app/2/reference/environment-variables/
-    envPrefix: ['VITE_', 'TAURI_'],
-    server: {
-      // Tauri requires a consistent port
-      strictPort: true,
-      hmr: {
-        // Use websocket for mobile hot reloading
-        protocol: 'ws',
-        // Make sure it's available on the network
-        host: '0.0.0.0',
-        // Use a specific port for hmr
-        port: 5183,
-      },
+  nitro: {
+    prerender: {
+      routes: ['/'],
     },
+  },
+
+  modules: ['@vite-pwa/nuxt'],
+  
+  pwa: {
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Sune',
+      short_name: 'Sune',
+      description: 'Description of Sune',
+      theme_color: '#000000',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: true,
+      suppressWarnings: false,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+
+    registerWebManifestInRouteRules: true,
   },
 });
