96e42d05b43886bfd1de7831f6abd969558c1340
lucasfelber <56022081+lucasfelber@users.noreply.github.com>
2024-09-19 11:03:52 +0200

    Avatar (#4)
    * avatar type and component

---
+++ app/components/Avatar.vue
@@ -0,0 +1,28 @@
+<template>
+    <span class="avatar" :class="userStatus">
+        L
+    </span>
+</template>
+
+<script setup lang="ts">
+const userStatus = ref<userStatus>("offline");
+</script>
+
+<style scoped>
+.avatar {
+    display: flex;
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+    justify-content: center;
+    border-radius: 100%;
+}
+
+.avatar.offline {
+    background-color: lightcoral;
+}
+
+.avatar.online {
+    background-color: lightgreen;
+}
+</style>
--- app/types/tiledata.d.ts
+++ app/types/tiledata.d.ts
@@ -1,5 +1,3 @@
-declare type Direction = "row" | "column"
-
 declare interface TileData {
     horizontal?: boolean,
     children?: TileData[]
